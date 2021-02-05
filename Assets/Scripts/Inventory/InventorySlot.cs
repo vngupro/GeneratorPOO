43a37936fd2cbebc963fa6fc7e9eb1125f25d7bb
Virginie Nguyen <vngu.pro@gmail.com>
2021-02-05 07:37:27 +0100

    bug fix

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -32,6 +32,11 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             {
                 player.StatsFrame.transform.position = (player.mousePos * new Vector2(1920, 1080)) - new Vector2(-150, 200);
             }
+
+            if(player.mousePos.y < 0.5f)
+            {
+                player.StatsFrame.transform.position = new Vector2(player.StatsFrame.transform.position.x, player.mousePos.y * 1080 + 200);
+            }
         }
     }
     public void OnPointerEnter(PointerEventData eventData)
